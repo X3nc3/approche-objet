e3ae265ca42bfc3c6308146cd530a7e7a2dd1444
Maxence <maxence.ogier@gmail.com>
2024-10-07 12:21:01 +0200

    tp-5

--- src/main/java/banque/entites/Compte.java
+++ src/main/java/banque/entites/Compte.java
@@ -5,8 +5,25 @@ public class Compte {
     private int numeroCompte;
     private double soldeCompte;
 
+    public int getNumeroCompte() {
+        return numeroCompte;
+    }
+
+    public void setNumeroCompte(int numeroCompte) {
+        this.numeroCompte = numeroCompte;
+    }
+
+    public double getSoldeCompte() {
+        return soldeCompte;
+    }
+
+    public void setSoldeCompte(double soldeCompte) {
+        this.soldeCompte = soldeCompte;
+    }
+
     public Compte(int numeroCompte, double soldeCompte) {
         this.numeroCompte = numeroCompte;
         this.soldeCompte = soldeCompte;
     }
+
 }
