95fc05010a7ca6423d1b9bfebf79b87acb6686a4
Maxence <maxence.ogier@gmail.com>
2024-10-07 11:08:07 +0200

    tp-4

---
+++ src/main/java/utils/testMethodeStatic.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class testMethodeStatic {
+
+    public static void main(String[] args) {
+
+        String chaine = "12";
+        int chaineConverti = Integer.parseInt(chaine);
+
+        int a = 10;
+        int b = 20;
+        int nombreMax = Integer.max(a, b);
+        System.out.println(nombreMax);
+
+    }
+}
