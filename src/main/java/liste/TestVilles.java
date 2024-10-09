c2a01ca0d118160c06f061e85b3c5e727bf45fba
Maxence <maxence.ogier@gmail.com>
2024-10-09 17:28:24 +0200

    tp-10

--- src/main/java/liste/TestVilles.java
+++ src/main/java/liste/TestVilles.java
@@ -1,6 +1,7 @@
 package liste;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class TestVilles {
@@ -16,42 +17,25 @@ public class TestVilles {
         listeVille.add(new Ville("Marseille", 850_700));
         listeVille.add(new Ville("Tarbes", 40_600));
 
-        int nbHabitantLePlusGrand = Integer.MIN_VALUE;
-        String VilleAvecLePlusHabitants = "";
+        Ville VilleMax = new Ville("", Integer.MIN_VALUE);
+        Ville villeMIn = new Ville("", Integer.MAX_VALUE);
+
         for (Ville ville : listeVille) {
-            if (ville.getNbHabitants() > nbHabitantLePlusGrand) {
-                nbHabitantLePlusGrand = ville.getNbHabitants();
-                VilleAvecLePlusHabitants = ville.getNom();
+            if (ville.getNbHabitants() > VilleMax.getNbHabitants()) {
+                VilleMax = ville;
+            } else if (ville.getNbHabitants() < villeMIn.getNbHabitants()) {
+                villeMIn = ville;
             }
-        }
-        System.out.println("La ville la plus peuplé est : " + VilleAvecLePlusHabitants);
-
-        System.out.println("");
-
-        int nbHabitantLePlusPetit= Integer.MAX_VALUE;
-        Ville VilleAvecLeMoinsHabitants = null;
-        for (Ville ville : listeVille) {
-            if (ville.getNbHabitants() < nbHabitantLePlusPetit) {
-                nbHabitantLePlusPetit = ville.getNbHabitants();
-                VilleAvecLeMoinsHabitants = ville;
-            }
-        }
-        listeVille.remove(VilleAvecLeMoinsHabitants);
-
-        for (Ville ville : listeVille) {
-            System.out.println("La liste modifié avec la ville la moins peuple en moins :" + ville);
-        }
-
-        System.out.println("");
-
-        for (int i=0; i<listeVille.size(); i++) {
-            if (listeVille.get(i).getNbHabitants() > 100_000) {
-                listeVille.get(i).setNom(listeVille.get(i).getNom().toUpperCase());
+            if (ville.getNbHabitants() > 100_000) {
+                ville.setNom(ville.getNom().toUpperCase());
             }
         }
 
-        for (Ville ville : listeVille) {
-            System.out.println("La liste modifié avec les ville de plus de 100 000 habiatnt en gras :" + ville);
-        }
+        listeVille.remove(villeMIn);
+
+        System.out.println("La ville avec plus d'habitant est  : " + VilleMax.getNom());
+        System.out.println("La ville avec plus d'habitant est  : " + villeMIn.getNom());
+        System.out.println(listeVille);
+
     }
 }
