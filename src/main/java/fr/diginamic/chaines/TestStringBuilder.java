d1c83ebee952897c43466515a4ebf165dd7aff8a
Maxence <maxence.ogier@gmail.com>
2024-10-14 10:47:52 +0200

    tp-16

---
+++ src/main/java/fr/diginamic/chaines/TestStringBuilder.java
@@ -0,0 +1,24 @@
+package fr.diginamic.chaines;
+
+public class TestStringBuilder {
+    public static void main(String[] args) {
+
+        StringBuilder builder = new StringBuilder();
+
+        long debut = System.currentTimeMillis();
+        for (int i=1; i<100_000; i++) {
+            builder.append(i);
+        }
+        long fin = System.currentTimeMillis();
+        System.out.println("Temps écoulé en millisecondes : " + (fin - debut));
+
+        String chiffre = "";
+
+        long debut2 = System.currentTimeMillis();
+        for (int i=1; i<100_000; i++) {
+            chiffre += i;
+        }
+        long fin2 = System.currentTimeMillis();
+        System.out.println("Temps écoulé en millisecondes : " + (fin2 - debut2));
+    }
+}
